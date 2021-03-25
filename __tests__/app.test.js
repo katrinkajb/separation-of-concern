ca6e71217bd0ef8405c85e28ae7d243b215bbd07
KB <katrinkajb@gmail.com>
2021-03-25 09:30:03 -0700

    put test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -66,4 +66,18 @@ describe('03_separation-of-concerns-demo routes', () => {
         });
       });
   });
+
+  it('updates an order in our database and sends a text message', () => {
+    return request(app)
+      .put('/api/v1/orders/1')
+      .send({ quantity: 10 })
+      .then((res) => {
+        // expect(createMessage).toHaveBeenCalledTimes(1);
+        expect(res.body).toEqual({
+          id: '1',
+          quantity: 10,
+        });
+      });
+  });
+
 })
