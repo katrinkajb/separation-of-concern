9c4d61b7a8e0f5757a353f73736a9dd8188a0fa6
KB <katrinkajb@gmail.com>
2021-03-25 10:03:54 -0700

    working put test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Order = require('../lib/models/Order');
+const twilio = require('twilio');
 
 jest.mock('twilio', () => () => ({
   messages: {
@@ -19,6 +20,8 @@ describe('03_separation-of-concerns-demo routes', () => {
   let order;
   beforeEach(async() => {
     order = await Order.insert({quantity: 5})
+
+    // twilio.sendSms.mockClear();
   })
 
   it('creates a new order in our database and sends a text message', () => {
@@ -26,7 +29,7 @@ describe('03_separation-of-concerns-demo routes', () => {
       .post('/api/v1/orders')
       .send({ quantity: 10 })
       .then((res) => {
-        // expect(createMessage).toHaveBeenCalledTimes(1);
+        // expect(twilio.sendSms).toHaveBeenCalledTimes(1);
         expect(res.body).toEqual({
           id: '2',
           quantity: 10,
@@ -72,12 +75,23 @@ describe('03_separation-of-concerns-demo routes', () => {
       .put('/api/v1/orders/1')
       .send({ quantity: 10 })
       .then((res) => {
-        // expect(createMessage).toHaveBeenCalledTimes(1);
+        // expect(twilio.sendSms).toHaveBeenCalledTimes(1);
         expect(res.body).toEqual({
           id: '1',
           quantity: 10,
         });
       });
   });
+
+  // it('ASYNC/AWAIT: updates an order in our database and sends a text message', async () => {
+  //   const res = await request(app)
+  //     .post('/api/v1/orders')
+  //     .send({ quantity: 5 });
+
+  //   expect(res.body).toEqual({
+  //     id: '2',
+  //     quantity: 5,
+  //   });
+  // });
 
 })
